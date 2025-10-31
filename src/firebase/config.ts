b1b56c6a63ac249c060b044e44c3a4c7f7ad9d8a
Arthur <amaralx48@gmail.com>
2025-10-31 01:32:29 +0000

    me esqueci de informar o buket: gs://studio-7095998453-3cbf3.firebasesto

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,6 +1,7 @@
 export const firebaseConfig = {
   "projectId": "studio-7095998453-3cbf3",
   "appId": "1:943560928567:web:cfe30236811e2aadf9feb2",
+  "storageBucket": "studio-7095998453-3cbf3.firebasestorage.app",
   "apiKey": "AIzaSyDsODegMZvxsDcQF3q2XvwSTgvELmRVrJo",
   "authDomain": "studio-7095998453-3cbf3.firebaseapp.com",
   "measurementId": "",
