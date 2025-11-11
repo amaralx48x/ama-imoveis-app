88a17ccefc1be12503fa1a99b1e948f4f8b9beeb
Arthur <amaralx48@gmail.com>
2025-11-11 01:50:46 +0000

    mudamos de projeto, esse nosso ambiente era uma copia do original, está

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,9 +1,8 @@
 export const firebaseConfig = {
-  "projectId": "studio-7095998453-3cbf3",
-  "appId": "1:943560928567:web:cfe30236811e2aadf9feb2",
-  "storageBucket": "studio-7095998453-3cbf3.firebasestorage.app",
-  "apiKey": "AIzaSyDsODegMZvxsDcQF3q2XvwSTgvELmRVrJo",
-  "authDomain": "studio-7095998453-3cbf3.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "943560928567"
+  apiKey: "AIzaSyCRXicBSuq6MkjT-07MJByc7TusU1V1Zpk",
+  authDomain: "ama-imveis-041125-945215-63275.firebaseapp.com",
+  projectId: "ama-imveis-041125-945215-63275",
+  storageBucket: "ama-imveis-041125-945215-63275.appspot.com",
+  messagingSenderId: "904973437127",
+  appId: "1:904973437127:web:2a29dbdde7f0d5bb6056e2"
 };
