c62eb56d2bcd1e99f1dbd7919539d33066347c2e
Arthur <amaralx48@gmail.com>
2025-11-11 13:17:30 +0000

    atualize src/firebase/config.ts : export const firebaseConfig = {   apiK

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -2,7 +2,7 @@ export const firebaseConfig = {
   apiKey: "AIzaSyCRXicBSuq6MkjT-07MJByc7TusU1V1Zpk",
   authDomain: "ama-imveis-041125-945215-63275.firebaseapp.com",
   projectId: "ama-imveis-041125-945215-63275",
-  storageBucket: "ama-imveis-041125-945215-63275.firebasestorage.app",
+  storageBucket: "ama-imveis-041125-945215-63275.appspot.com",
   messagingSenderId: "904973437127",
   appId: "1:904973437127:web:2a29dbdde7f0d5bb6056e2"
 };
