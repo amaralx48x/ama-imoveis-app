7e58bc50a1e54fe0641bbec9e543d75b2925bd4e
Arthur <amaralx48@gmail.com>
2025-11-21 16:53:27 +0000

    Runtime Error
    Error: FormDescription is not defined
    
    src/app/admin/dem

--- src/app/admin/demo-account/page.tsx
+++ src/app/admin/demo-account/page.tsx
@@ -1,3 +1,4 @@
+
 'use client';
 
 import { useForm } from 'react-hook-form';
@@ -7,6 +8,7 @@ import { Button } from '@/components/ui/button';
 import {
   Form,
   FormControl,
+  FormDescription,
   FormField,
   FormItem,
   FormLabel,
