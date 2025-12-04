87247e64d4937db0062f376239cceb188ebf8ef5
Arthur <amaralx48@gmail.com>
2025-12-04 13:07:02 +0000

    Agora temos 100% de certeza do que está acontecendo.
    Você fez tudo cert

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,9 +1,9 @@
 
 export const firebaseConfig = {
   apiKey: "AIzaSyDsODegMZvxsDcQF3q2XvwSTgvELmRVrJo",
-  authDomain: "ama-imveis-041125-945215-63275.firebaseapp.com",
+  authDomain: "studio-7095998453-3cbf3.firebaseapp.com",
   projectId: "studio-7095998453-3cbf3",
   storageBucket: "studio-7095998453-3cbf3.firebasestorage.app",
   messagingSenderId: "943560928567",
-  appId: "1:943560928567:web:cfe30236811e2aadf9feb2"
+  appId: "1:943560928567:web:79196a25d8f79608f9feb2"
 };
