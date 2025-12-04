52c5270222d9c0f90e54f7419ca000f4dae9c43a
Arthur <amaralx48@gmail.com>
2025-12-04 03:50:11 +0000

    AGOA ESTÁ BUSCANDO NO PROJETO ERRADO, POIS NÃO CAREGA OS USUAIROS NEM IM

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -2,8 +2,8 @@
 export const firebaseConfig = {
   apiKey: "AIzaSyDsODegMZvxsDcQF3q2XvwSTgvELmRVrJo",
   authDomain: "ama-imveis-041125-945215-63275.firebaseapp.com",
-  projectId: "ama-imveis-041125-945215-63275",
-  storageBucket: "ama-imveis-041125-945215-63275.appspot.com",
+  projectId: "studio-7095998453-3cbf3",
+  storageBucket: "studio-7095998453-3cbf3.firebasestorage.app",
   messagingSenderId: "943560928567",
   appId: "1:943560928567:web:cfe30236811e2aadf9feb2"
 };
