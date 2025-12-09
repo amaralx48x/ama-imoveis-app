ec4807a8e43b041488ea005ff4468199acdce906
Arthur <amaralx48@gmail.com>
2025-12-09 20:53:00 +0000

    o logar com google ainda falha: Download the React DevTools for a better

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,10 +1,11 @@
 
 export const firebaseConfig = {
   apiKey: "AIzaSyDsODegMZvxsDcQF3q2XvwSTgvELmRVrJo",
-  authDomain: null,
+  authDomain: "studio-7095998453-3cbf3.firebaseapp.com",
   databaseURL: "https://studio-7095998453-3cbf3-default-rtdb.firebaseio.com",
   projectId: "studio-7095998453-3cbf3",
   storageBucket: "studio-7095998453-3cbf3.firebasestorage.app",
   messagingSenderId: "943560928567",
   appId: "1:943560928567:web:79196a25d8f79608f9feb2"
 };
+
